23dc4cb103de74502f47625bcfbb31a0d8d7e972
Semen Kataev <skataev@avito.ru>
2022-08-02 19:06:27 +0300

    lesson 7

---
+++ ex7/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type TestRow struct {
+	Title string  `bson:"title"`
+	Price float64 `bson:"price"`
+}
+
+/**
+Фоново нужно запустить базу данных
+> docker run -ti --network=host -p 27017:27017 -d mongo:latest
+> mongo --host=127.0.0.1
+*/
+
+func main() {
+	ctx := context.Background()
+
+	mongoDB, err := mongo.NewClient(
+		options.Client().ApplyURI("mongodb://192.168.64.165:27017"),
+	)
+	err = mongoDB.Connect(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	findOption := options.Find()
+	findOption.SetSort(bson.D{{"_id", 1}})
+	cur, err := mongoDB.Database("marketplace").Collection("goods").Find(ctx, bson.D{}, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer cur.Close(ctx)
+	for cur.Next(ctx) {
+		var row TestRow
+		err := cur.Decode(&row)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("Информация о товаре")
+		fmt.Println(fmt.Sprintf("Название %s", row.Title))
+		fmt.Println(fmt.Sprintf("Цена %f", row.Price))
+	}
+}
