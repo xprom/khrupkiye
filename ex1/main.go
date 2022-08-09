4c7066529595c96a1d066d95234469c7c5e1b8aa
Van <vanmail89@yandex.ru>
2022-08-09 17:46:02 +0300

    Приветствие

--- ex1/main.go
+++ ex1/main.go
@@ -3,5 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World, from serednitsky")
+	for true {
+		var myName string
+		var myStatus string
+		var varBreak bool = false
+
+		fmt.Println("Как тебя зовут?")
+		fmt.Scan(&myName)
+		fmt.Println("Привет " + myName + "!")
+
+		fmt.Println("Как твои дела?")
+		fmt.Scan(&myStatus)
+		switch myStatus {
+		case "Хорошо", "хорошо":
+			{
+				fmt.Println("Отлично!")
+				varBreak = true
+			}
+		case "Плохо", "плохо":
+			{
+				fmt.Println("Печально...")
+				varBreak = true
+			}
+		default:
+			{
+				fmt.Println("Не понял ответа... Давай повторим!")
+			}
+		}
+
+		if varBreak == true {
+			break
+		}
+	}
+
+	fmt.Println("Удачного дня!")
 }
